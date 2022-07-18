4961926ace50c5b367b71ab0b332ea1b208ecd08
Jack Henhapl <jack.henhapl@gmail.com>
2022-07-18 01:27:06 -0400

    Adding code to send/receive observations

--- src/main/java/edu/whimc/photographer/Photographer.java
+++ src/main/java/edu/whimc/photographer/Photographer.java
@@ -4,23 +4,31 @@ import com.gmail.filoghost.holographicdisplays.api.Hologram;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+import edu.whimc.observations.Observations;
 import edu.whimc.observations.models.ObserveEvent;
+import edu.whimc.observations.models.Observation;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+
+import edu.whimc.photographer.socket.Response;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Photographer extends JavaPlugin implements Listener {
 
     private SocketIOServer server;
     private Map<UUID, UUID> photographers = new HashMap<>();
+    private Observations observationsPlugin;
 
     @Override
     public void onEnable() {
@@ -29,6 +37,8 @@ public final class Photographer extends JavaPlugin implements Listener {
         Configuration config = new Configuration();
         config.setHostname(super.getConfig().getString("websocket.host"));
         config.setPort(super.getConfig().getInt("websocket.port"));
+
+        this.observationsPlugin = (Observations) getServer().getPluginManager().getPlugin("WHIMC-Observations");
 
         this.server = new SocketIOServer(config);
         this.server.addConnectListener(client -> {
@@ -41,10 +51,16 @@ public final class Photographer extends JavaPlugin implements Listener {
         this.server.addDisconnectListener(
                 client -> Bukkit.broadcastMessage("disconnected " + client.getRemoteAddress()));
 
-        server.addEventListener("test", String.class,
-                (client, message, ackRequest) -> {
-                    Bukkit.broadcastMessage(client.getRemoteAddress() + " [" + client.get("uuid") + "]: " + message);
-                });
+        server.addEventListener("test", String.class, (client, message, ackRequest) ->
+            Bukkit.broadcastMessage(client.getRemoteAddress() + " [" + client.get("uuid") + "]: " + message)
+        );
+
+        server.addEventListener("screenshot_response", Response.class, (client, response, ackRequest) -> {
+            Bukkit.broadcastMessage("Observation ID: " + response.getObservationId());
+            Bukkit.broadcastMessage("Feedback: " + response.getFeedback());
+            Bukkit.broadcastMessage("Generated caption: " + response.getGeneratedCaption());
+            Bukkit.broadcastMessage("Score: " + response.getScore());
+        });
 
         this.server.start();
 
@@ -59,18 +75,28 @@ public final class Photographer extends JavaPlugin implements Listener {
                 continue;
             }
 
-            player.sendMessage("An observation has been made! Teleporting you to take a picture in 5 second");
+            player.sendMessage("An observation has been made! Teleporting you to take a picture in 3 seconds");
 
             Bukkit.getScheduler().runTaskLater(this, () -> {
-                Hologram hologram = event.getObservation().getHologram();
-                hologram.getVisibilityManager().hideTo(player);
-                player.teleport(event.getObservation().getViewLocation());
+                Observation observation = event.getObservation();
+
+                // Make the observation invisible
+                for (Hologram hologram : HologramsAPI.getHolograms(this.observationsPlugin)) {
+                    hologram.getVisibilityManager().hideTo(player);
+                }
+                player.teleport(observation.getViewLocation());
 
                 UUID clientUuid = this.photographers.get(player.getUniqueId());
                 SocketIOClient client = getClient(clientUuid);
 
-                client.sendEvent("screenshot");
-            }, 20 * 5);
+                String strippedObservation = ChatColor.stripColor(observation.getObservation());
+
+                player.sendMessage("Taking picture in 5 seconds");
+                Bukkit.getScheduler().runTaskLaterAsynchronously(this, () -> {
+                    client.sendEvent("screenshot", observation.getId(), strippedObservation);
+                }, 20 * 5);
+
+            }, 20 * 3);
         }
     }
 
@@ -116,7 +142,7 @@ public final class Photographer extends JavaPlugin implements Listener {
                 sender.sendMessage("You have to be a player");
             }
             Player player = (Player) sender;
-            player.sendMessage("You have become a photographer for uuid!");
+            player.sendMessage("You have become a photographer for " + client.get("uuid"));
             this.photographers.put(player.getUniqueId(), client.get("uuid"));
             return true;
         }
