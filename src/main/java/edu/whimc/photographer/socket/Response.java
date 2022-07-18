dc31b8a73a262a5567f4d0215c94ed529044f15b
Jack Henhapl <jack.henhapl@gmail.com>
2022-07-18 01:27:24 -0400

    Untested: Response object from client

---
+++ src/main/java/edu/whimc/photographer/socket/Response.java
@@ -0,0 +1,40 @@
+package edu.whimc.photographer.socket;
+
+import java.util.UUID;
+
+public class Response {
+
+    private UUID clientUuid;
+    private int observationId;
+    private String feedback;
+    private String generatedCaption;
+    private float score;
+
+    public Response(UUID clientUuid, int observationId, String feedback, String generatedCaption, float score) {
+        this.clientUuid = clientUuid;
+        this.observationId = observationId;
+        this.feedback = feedback;
+        this.generatedCaption = generatedCaption;
+        this.score = score;
+    }
+
+    public UUID getClientUuid() {
+        return clientUuid;
+    }
+
+    public int getObservationId() {
+        return observationId;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public String getGeneratedCaption() {
+        return generatedCaption;
+    }
+
+    public float getScore() {
+        return score;
+    }
+}
